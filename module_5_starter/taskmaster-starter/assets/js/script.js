68c8b6533914f1fbbb29749981bae82cdb20d97f
Shmee <smaheilman@gmail.com>
2021-09-22 22:52:48 -0400

    UX enhanecments

--- module_5_starter/taskmaster-starter/assets/js/script.js
+++ module_5_starter/taskmaster-starter/assets/js/script.js
@@ -155,13 +155,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper:"clone",
   activate: function(event) {
+    $(this).addClass("dropover");
     console.log("activate", this)
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
     console.log("deactivate", this)
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     console.log("over", event.target);
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data in
@@ -201,12 +209,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -234,6 +245,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days"))<=2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(auditTask);
 };
 
 // modal was triggered
@@ -249,7 +261,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -282,4 +294,9 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
